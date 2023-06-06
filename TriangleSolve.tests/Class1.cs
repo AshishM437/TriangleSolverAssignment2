cdac0d3769aa026b71d8b1bdc60886f9a6d31743
Pintu Ashish <pintuashish@Pintus-MacBook.local>
2023-06-06 07:33:57 +0530

    Scalene triangle tests

--- TriangleSolve.tests/Class1.cs
+++ TriangleSolve.tests/Class1.cs
@@ -67,6 +67,108 @@ namespace TriangleSolve.tests
                 // Assert
                 Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
             }
+             [Test]
+            public void IsoscelesTriangle_ValidResponse_Test1()
+            {
+                // Arrange
+                int a = 7, b = 7, c = 5;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
+            }
+
+            [Test]
+            public void IsoscelesTriangle_ValidResponse_Test2()
+            {
+                // Arrange
+                int a = 5, b = 6, c = 6;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
+            }
+
+            [Test]
+            public void IsoscelesTriangle_ValidResponse_Test3()
+            {
+                // Arrange
+                int a = 8, b = 6, c = 8;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
+            }
+             [Test]
+            public void ScaleneTriangle_ValidResponse_Test1()
+            {
+                // Arrange
+                int a = 3, b = 4, c = 5;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is a SCALENE", result);
+            }
+
+            [Test]
+            public void ScaleneTriangle_ValidResponse_Test2()
+            {
+                // Arrange
+                int a = 7, b = 9, c = 11;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is a SCALENE", result);
+            }
+
+            [Test]
+            public void ScaleneTriangle_ValidResponse_Test3()
+            {
+                // Arrange
+                int a = 5, b = 12, c = 13;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is a SCALENE", result);
+            }
+
+            [Test]
+            public void ScaleneTriangle_ValidResponse_Test4()
+            {
+                // Arrange
+                int a = 10, b = 11, c = 12;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is a SCALENE", result);
+            }
+
+            [Test]
+            public void ScaleneTriangle_ValidResponse_Test5()
+            {
+                // Arrange
+                int a = 2, b = 4, c = 5;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is a SCALENE", result);
+            }
            [Test]
             public void InvalidResponse_InvalidResponse_Test1()
             {
