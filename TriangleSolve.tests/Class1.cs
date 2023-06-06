f650a39e5fbea25d77d3725345251235b0de9c97
Pintu Ashish <pintuashish@Pintus-MacBook.local>
2023-06-06 07:21:13 +0530

    intial commit

---
+++ TriangleSolve.tests/Class1.cs
@@ -0,0 +1,216 @@
+﻿using NUnit.Framework;
+using TriangleSolverAssignment2;
+
+namespace TriangleSolve.tests
+{
+    [TestFixture]
+    public class TriangleTest
+    {
+        private Triangle triangle;
+
+        [SetUp]
+        public void Setup()
+        {
+            triangle = new Triangle();
+        }
+
+        [TestFixture]
+        public class TriangleTests
+        {
+            [Test]
+            public void EquilateralTriangle_ValidResponse_Test()
+            {
+                // Arrange
+                int a = 5, b = 5, c = 5;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is an EQUILATERAL", result);
+            }
+
+            [Test]
+            public void IsoscelesTriangle_ValidResponse_Test1()
+            {
+                // Arrange
+                int a = 7, b = 7, c = 5;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
+            }
+
+            [Test]
+            public void IsoscelesTriangle_ValidResponse_Test2()
+            {
+                // Arrange
+                int a = 5, b = 6, c = 6;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
+            }
+
+            [Test]
+            public void IsoscelesTriangle_ValidResponse_Test3()
+            {
+                // Arrange
+                int a = 8, b = 6, c = 8;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
+            }
+
+            [Test]
+            public void ScaleneTriangle_ValidResponse_Test1()
+            {
+                // Arrange
+                int a = 3, b = 4, c = 5;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is a SCALENE", result);
+            }
+
+            [Test]
+            public void ScaleneTriangle_ValidResponse_Test2()
+            {
+                // Arrange
+                int a = 7, b = 9, c = 11;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is a SCALENE", result);
+            }
+
+            [Test]
+            public void ScaleneTriangle_ValidResponse_Test3()
+            {
+                // Arrange
+                int a = 5, b = 12, c = 13;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is a SCALENE", result);
+            }
+
+            [Test]
+            public void ScaleneTriangle_ValidResponse_Test4()
+            {
+                // Arrange
+                int a = 10, b = 11, c = 12;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is a SCALENE", result);
+            }
+
+            [Test]
+            public void ScaleneTriangle_ValidResponse_Test5()
+            {
+                // Arrange
+                int a = 2, b = 4, c = 5;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("The triangle is valid and is a SCALENE", result);
+            }
+
+            [Test]
+            public void ZeroLengthSide_InvalidResponse_Test1()
+            {
+                // Arrange
+                int a = 0, b = 4, c = 5;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("At least one side of your triangle has a zero length and is thus invalid", result);
+            }
+
+            [Test]
+            public void ZeroLengthSide_InvalidResponse_Test2()
+            {
+                // Arrange
+                int a = 3, b = 0, c = 5;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("At least one side of your triangle has a zero length and is thus invalid", result);
+            }
+
+            [Test]
+            public void ZeroLengthSide_InvalidResponse_Test3()
+            {
+                // Arrange
+                int a = 3, b = 4, c = 0;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("At least one side of your triangle has a zero length and is thus invalid", result);
+            }
+
+            [Test]
+            public void InvalidResponse_InvalidResponse_Test1()
+            {
+                // Arrange
+                int a = 2, b = 3, c = 10;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("A triangle cannot be formed with those numbers", result);
+            }
+
+            [Test]
+            public void InvalidResponse_InvalidResponse_Test2()
+            {
+                // Arrange
+                int a = 1, b = 1, c = 3;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("A triangle cannot be formed with those numbers", result);
+            }
+
+            [Test]
+            public void InvalidResponse_InvalidResponse_Test3()
+            {
+                // Arrange
+                int a = 5, b = 9, c = 2;
+
+                // Act
+                string result = Triangle.AnalyzeTriangle(a, b, c);
+
+                // Assert
+                Assert.AreEqual("A triangle cannot be formed with those numbers", result);
+            }
+        }
+    }
+}
---
+++ TriangleSolverAssignment2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TriangleSolverAssignment2;
+
+Console.WriteLine("Enter firstSide");
+int firstSide = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter secondSide");
+int secondSide = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter thirdSide");
+int thirdSide = Convert.ToInt32(Console.ReadLine());
+string triangleType = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide); ;
+Console.WriteLine("Triangle Type: " + triangleType);
+
